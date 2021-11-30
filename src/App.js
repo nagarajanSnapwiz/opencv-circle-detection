23d198fbfd376bed4de90a4076e80348eb7f29dd
nagarajanSnapwiz <nagarajan.natarajan@snapwiz.com>
2021-11-30 20:57:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { useOpenCv } from 'opencv-react';
 export default function App() {
   const { cv, loaded: cvLoaded } = useOpenCv();
 
-  if (!(cvLoaded && cv)) {
-    return <h2>Loading...</h2>;
-  }
-  console.log('cvload', { cv, cvLoaded });
   return (
     <main>
       <div className="row">
