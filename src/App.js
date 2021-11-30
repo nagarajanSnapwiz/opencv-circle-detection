ba5bd66cb3e8e6497da373901719d4ff6989923e
nagarajanSnapwiz <nagarajan.natarajan@snapwiz.com>
2021-11-30 21:18:05 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useOpenCv } from 'opencv-react';
+import testData from './data.json';
 
 export default function App() {
   const { cv, loaded: cvLoaded } = useOpenCv();
+  const [selectedFile, setSelectedFile] = useState(
+    'https://cdn.jsdelivr.net/gh/nagarajanSnapwiz/opencv-circle-detection@master/public/images/big1.jpg'
+  );
 
+  if (!cvLoaded) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <main>
       <div className="row">
-        <select>
-          <option>large1</option>
-          <option>small1</option>
+        <select
+          value={selectedFile}
+          onChange={(e) => {
+            setSelectedFile(e.target.value);
+          }}
+        >
+          {testData.map((x) => (
+            <option value={x.url}>{x.name}</option>
+          ))}
         </select>
       </div>
-      <h2>Image t </h2>
+      <h2>Original Image </h2>
+      <img src={selectedFile} style={{ maxWidth: '100%' }} />
     </main>
   );
 }
